65211a1e229953636a5e2f942f959a8f3fd233d0
Zhenkaaf <zhenkaaf@gmail.com>
2024-01-08 22:07:59 +0200

    imageSRCremotePatterns

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Image from "next/image";
+import s from "./about.module.css";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={s.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,36 +1,67 @@
-import Link from "next/link";
-import React from "react";
+"use client";
+import { useState } from "react";
 import s from "./links.module.css";
 import NavLink from "./navLink/navLink";
 
+const links = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
+
 const Links = () => {
-  const links = [
-    {
-      title: "Homepage",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+  const [open, setOpen] = useState(false);
+
+  const session = true;
+  const isAdmin = true;
 
   return (
-    <div className={s.links}>
-      {links.map((link) => (
-        <NavLink
-          item={link}
-          key={link.title}
-        />
-      ))}
+    <div className={s.container}>
+      <div className={s.links}>
+        {links.map((link) => (
+          <NavLink
+            item={link}
+            key={link.title}
+          />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={s.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <button
+        onClick={() => setOpen((prev) => !prev)}
+        className={s.menuButton}
+      >
+        Menu
+      </button>
+      {open && (
+        <div className={s.mobileLinks}>
+          {links.map((link) => (
+            <NavLink
+              item={link}
+              key={link.title}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
